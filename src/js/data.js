427c1f57220cfa23d5d08affc35b2f3459d24b13
unknown <dlauralr@icloud.com>
2018-08-06 14:09:22 -0500

    Modificando funciones ecmascript 6

--- src/js/data.js
+++
@@ -1,7 +0,0 @@
-window.onload=timeout;
-function timeout(){
-window.setTimeout("redirect()",5000)}
-
-function redirect(){
-window.location="views/start.html"
-return}
---
+++ src/js/splash.js
@@ -0,0 +1,8 @@
+const timeout = () => {
+window.setTimeout("redirect()",2000)}
+
+window.onload = timeout;
+
+const redirect = () => {
+window.location="views/start.html"
+return}
