419d9818b419ae3d43914e1c0840503dbb8990cb
unknown <dlauralr@icloud.com>
2018-08-06 20:22:13 -0500

    Agregando geolocalizacion

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
-#  F O O D M A P
+#  Time To Eat
 
-Crea una web-app que a través de un input pueda filtrar los restaurantes
-que se encuentran cerca de ti *(Tú decides la estructura que tendrán tus datos,
-puedes crear una lista de restaurantes en un arreglo, en un objeto, consumir una API, etc. Lo importante es que el contenido debe ser dinámico y no estático y el diseño es totalmente libre)*.
+Bienvenido a la web-app que te muestra los mejores lugares para comer cerca de ti *(Actualmente, existen innumerables aplicaciones de comida, tanto saludables como de aprendizaje y sociales, cada una con un objetivo específico)*.
 
 #### Flujo de la aplicación.
 
--- src/js/map.js
+++ src/js/map.js
@@ -1,7 +1,29 @@
-var map;
+btnlocation.addEventListener('click', e => {
+  const map = document.getElementById('map');
+
+  //Obtiene latitude y longitude
+  const location = (position) => {
+    let latitude = position.coords.latitude;
+    let longuitude = position.coords.longitude;
+    //Agrega latitude y longitude a mapa
+    const newmap = new google.maps.Map(map, {
+      center: {lat: latitude, lng: longuitude},
+      zoom: 8
+    })
+  }
+
+  const error = () => {
+    map.innerHTML = 'No se pudo obtener tu ubicación';
+  }
+
+  navigator.geolocation.getCurrentPosition(location, error);
+
+});
+
+/*var map;
       function initMap() {
         map = new google.maps.Map(document.getElementById('map'), {
           center: {lat: -34.397, lng: 150.644},
           zoom: 8
         });
-      }
+      }*/
